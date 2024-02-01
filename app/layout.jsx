b6f0635275462e036efffbd8e4db1cf7ecaee3e5
Jeon SunYong <sunyong0725@naver.com>
2024-02-01 18:41:50 +0900

    📁 layout 정리

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,10 +2,10 @@ import { Inter } from "next/font/google";
 import "/styles/globals.css";
 import Header from "@/components/header/Header";
 import Footer from "@/components/footer/Footer";
-
+import { Outfit } from "next/font/google";
 import { ThemeProvider } from "@/components/Theme/ThemeProvider";
 
-const inter = Inter({ subsets: ["latin"] });
+const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata = {
   title: "SunYong Portfolio",
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body className={outfit.className}>
         <ThemeProvider attribute="class" defaultTheme="light">
           <Header />
           {children}
