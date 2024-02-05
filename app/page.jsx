ab41e72ab842dd7f7f51dbf256f7d03b410b3c0b
Jeon SunYong <sunyong0725@naver.com>
2024-02-05 21:30:00 +0900

    📁 Services 정리

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,14 @@
 import Introduction from "@/components/introduction/Introduction";
 import About from "@/components/about/About";
+import Services from "@/components/Services/Services";
+import Work from '@/components/Work/Work'
 export default function Home() {
   return (
     <main>
       <Introduction />
       <About />
+      <Services />
+      <Work />
     </main>
   );
 }
