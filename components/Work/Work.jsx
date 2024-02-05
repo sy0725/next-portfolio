912368a81fdd5b4f614d974044ea4741ef59a873
Jeon SunYong <sunyong0725@naver.com>
2024-02-05 21:30:05 +0900

    📁 Work 정리

---
+++ components/Work/Work.jsx
@@ -0,0 +1,21 @@
+"use client";
+import Link from "next/Link";
+import { Button } from "../ui/button";
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper/modules";
+
+import ProjectCard from "@/components/Work/ProjectCard";
+
+const ProjectData = [
+  {
+    image: "",
+  },
+];
+
+const Work = () => {
+  return <div>Work</div>;
+};
+
+export default Work;
